a97a2814d5d9ff49b8db3ca0727e8f17cab9858d
Chcndr <94785821+Chcndr@users.noreply.github.com>
2025-11-23 12:58:03 -0500

    fix: Corretto errore di sintassi "Unterminated string literal" in server/routers.ts

--- server/routers.ts
+++ server/routers.ts
@@ -9,8 +9,7 @@ import { mihubRouter } from "./mihubRouter";
 
 export const appRouter = router({
     // if you need to use socket.io, read and register route in server/_core/index.ts, all api should start with 
-'/api/
-' so that the gateway can route correctly
+// '/api/' so that the gateway can route correctly
   system: systemRouter,
   auth: router({
     me: publicProcedure.query(opts => opts.ctx.user),
